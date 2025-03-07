6896e17dc7be179f0a5224952cd1c249f9b68941
Victormrf <vmrf2000@hotmail.com>
2025-03-07 17:06:26 -0300

    improved visuals

--- components/magicui/meteors.tsx
+++ components/magicui/meteors.tsx
@@ -47,7 +47,7 @@ export const Meteors = ({
           key={idx}
           style={{ ...style }}
           className={cn(
-            "pointer-events-none absolute size-0.5 rotate-[var(--angle)] animate-meteor rounded-full bg-zinc-500 shadow-[0_0_0_1px_#73e8d4]",
+            "pointer-events-none absolute size-0.5 rotate-[var(--angle)] animate-meteor rounded-full bg-zinc-500 shadow-[0_0_0_1px_#215d59] dark:shadow-[0_0_0_1px_#73e8d4]",
             className
           )}
         >
