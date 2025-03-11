16602f410ab97203ef88795c7727b6d515d70e4b
Victormrf <vmrf2000@hotmail.com>
2025-03-11 03:36:15 -0300

    Improved footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,17 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="relative lg:w-8/12 md:w-7/12 sm:w-full mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] overflow-hidden">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full overflow-hidden">
-        <Meteors />
-        <Intro />
-        <Skills />
-        <CustomTab />
-        <Footer />
-        <CustomDock orientation="vertical" />
-      </main>
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <div className="relative lg:w-8/12 md:w-7/12 sm:w-full mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] overflow-hidden">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full overflow-hidden">
+          <Meteors />
+          <Intro />
+          <Skills />
+          <CustomTab />
+          <CustomDock orientation="vertical" />
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
