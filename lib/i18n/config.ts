989fbe6ffffff5167f1e690320267d8ae9715a04
Victormrf <vmrf2000@hotmail.com>
2025-03-23 17:00:58 -0300

    Adjusted intro

--- lib/i18n/config.ts
+++ lib/i18n/config.ts
@@ -19,7 +19,7 @@ i18n.use(initReactI18next).init({
       translation: {
         greeting: "Hi! I'm",
         intro:
-          "As a Software Engineer with 2+ years of experience, i have crafted solutions for backend and frontend development, creating applications from scratch to production-ready. I have experience working with relational and non-relational databases. I also hold a degree in electrical engineering, and currently am a post graduate student in software engineering. As a software development enthusiast, I'm always looking to acquire new knowledge in the field and develop new technical skills.",
+          "As a Software Engineer with 2+ years of experience, i have crafted solutions for backend and frontend development, creating applications from scratch to production-ready. I have experience working with relational and non-relational databases, including optimized search engines. I also hold a degree in electrical engineering, and currently am a post graduate student in software engineering. As a software development enthusiast, I'm always looking to acquire new knowledge in the field and develop new technical skills.",
         downloadCV: "Download CV",
         projectsTab: "Projects",
         workExperienceTab: "Work Experience",
@@ -38,7 +38,7 @@ i18n.use(initReactI18next).init({
       translation: {
         greeting: "Olá! Eu sou",
         intro:
-          "Como Engenheiro de Software com mais de 2 anos de experiência, desenvolvi soluções para desenvolvimento backend e frontend, criando aplicações do zero até prontas para produção. Tenho experiência trabalhando com bancos de dados relacionais e não relacionais. Também sou formado em engenharia elétrica e atualmente sou pós-graduando em engenharia de software. Como entusiasta do desenvolvimento de software, estou sempre buscando adquirir novos conhecimentos na área e desenvolver novas habilidades técnicas.",
+          "Atuando como Engenheiro de Software a mais de 2 anos, desenvolvi soluções de desenvolvimento backend e frontend, criando aplicações do zero até produção. Possuo experiência prática trabalhando com bancos de dados relacionais e não relacionais, inclusive com o uso de search engines otimizadoras. Possuo formação em engenharia elétrica e atualmente sou pós-graduando em engenharia de software. Como entusiasta de desenvolvimento de software, estou sempre buscando adquirir novos conhecimentos na área e desenvolver novas habilidades técnicas.",
         downloadCV: "Baixar currículo",
         projectsTab: "Projetos",
         workExperienceTab: "Experiência Profissional",
@@ -50,7 +50,7 @@ i18n.use(initReactI18next).init({
         view: "Ir para aplicação",
         code: "Código",
         ...educationPT,
-        contactMe: "Bora trocar uma ideia? Meu e-mail é",
+        contactMe: "Bora trocar uma ideia? Você pode me encontrar em",
       },
     },
   },
